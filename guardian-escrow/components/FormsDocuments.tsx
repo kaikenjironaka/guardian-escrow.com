7efc69bcc35ee5fd4170b65bd85d1a31494944ea
kaikenjironaka <126515743+kaikenjironaka@users.noreply.github.com>
2023-03-23 10:55:08 -0700

    Update FormsDocuments.tsx

--- guardian-escrow/components/FormsDocuments.tsx
+++ guardian-escrow/components/FormsDocuments.tsx
@@ -30,13 +30,13 @@ return (
             {t.formsdocument.title}
       </h2>
       <ul class="space-y-2 list-disc">
-        <li class="text-justify text-cyan-400"><a href={asset("/GuardianCollectionEscrowForm2023.pdf")}>{t.formsdocument.escrowcollectionpdf}</a></li>
-        <li class="text-justify"><a href={asset("/Automatic Deposit Form.pdf")}>{t.formsdocument.autodeposit}</a></li>
-        <li class="text-justify"><a href={asset("/Automatic Payment Form.pdf")}>{t.formsdocument.autopaymentform}</a></li>
-        <li class="text-justify"><a href={asset("/GUARDIANCustomEscrow.pdf")}>{t.formsdocument.customescrow}</a></li>
-        <li class="text-justify"><a href={asset("/CONSTESCROW1-2.pdf")}>{t.formsdocument.contructionescrow}</a></li>
-        <li class="text-justify"><a href={asset("/DRAWREQUEST.pdf")}>{t.formsdocument.drawreqeustspdf}</a></li>
-        <li class="text-justify"><a href={asset("/ESCROW DEPOSITORY INSTRUCTIONS.doc")}>{t.formsdocument.softwareescrow}</a></li>
+        <li class="text-justify text-cyan-500 underline-offset-1"><a href={asset("/GuardianCollectionEscrowForm2023.pdf")}>{t.formsdocument.escrowcollectionpdf}</a></li>
+        <li class="text-justify text-cyan-500 underline-offset-1"><a href={asset("/Automatic Deposit Form.pdf")}>{t.formsdocument.autodeposit}</a></li>
+        <li class="text-justify text-cyan-500 underline-offset-1"><a href={asset("/Automatic Payment Form.pdf")}>{t.formsdocument.autopaymentform}</a></li>
+        <li class="text-justify text-cyan-500 underline-offset-1"><a href={asset("/GUARDIANCustomEscrow.pdf")}>{t.formsdocument.customescrow}</a></li>
+        <li class="text-justify text-cyan-500 underline-offset-1"><a href={asset("/CONSTESCROW1-2.pdf")}>{t.formsdocument.contructionescrow}</a></li>
+        <li class="text-justify text-cyan-500 underline-offset-1"><a href={asset("/DRAWREQUEST.pdf")}>{t.formsdocument.drawreqeustspdf}</a></li>
+        <li class="text-justify text-cyan-500 underline-offset-1"><a href={asset("/ESCROW DEPOSITORY INSTRUCTIONS.doc")}>{t.formsdocument.softwareescrow}</a></li>
       </ul>
     </div>
   </section>
